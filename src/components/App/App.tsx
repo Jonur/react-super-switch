c6fdc394016e8aa2ca86c6033a62cfaf79a6f9af
Dimitris Damilos <d.k.damilos@gmail.com>
2023-04-07 20:12:11 +0100

    Add more info to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,20 +3,28 @@ import "./App.css";
 import SuperSwitch from "../SuperSwitch";
 import Option from "../Option";
 
-const App = () => {
+const App: React.FC = () => {
   const truthyCondition = 1 + 1 === 2;
   const falsyCondition = 2 + 2 === 3;
 
   return (
     <div className="App">
       <SuperSwitch>
-        <Option condition={truthyCondition}>This should be displayed.</Option>
-        <Option condition={falsyCondition}>
-          This should NOT be displayed.
+        <Option condition={truthyCondition}>
+          <span data-testid="truthy-condition">This should be displayed.</span>
         </Option>
+
+        <Option condition={falsyCondition}>
+          <span data-testid="falsy-condition">
+            This should NOT be displayed.
+          </span>
+        </Option>
+
         <Option condition={truthyCondition} default>
-          This is the default. It is visible when every other option has a falsy
-          condition.
+          <span data-testid="default">
+            This is the default. It is visible when every other option has a
+            falsy condition.
+          </span>
         </Option>
       </SuperSwitch>
     </div>
